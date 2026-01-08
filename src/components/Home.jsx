8c9eb75f08ac6856389154abcce3c0b5da692182
Krishnakant Mane <krishnamane712c@gmail.com>
2026-01-08 17:09:40 +0530

    UI change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,38 +17,41 @@ export const Home = () => {
 
   return (
     <>
-      <div className='w-full h-screen border '>
+      <div className="w-full min-h-screen border">
         <span>{User.username}</span>
-        <div className='flex flex-wrap w-full p-10 gap-5'>
-          {
-            videos.map((items) => (
-              <div
-                key={items.id}
-                className="
-                rounded-2xl p-3 overflow-hidden shadow
-                transform transition-transform duration-300
-                hover:-translate-y-2
-                "
-              >
-                <div className="flex flex-col">
-                  <h1 className='font-bold'>{items.id}</h1>
-                  <h1>{items.name}</h1>
+
+        <div className="flex flex-wrap w-full p-4 sm:p-6 lg:p-10 gap-5">
+          {videos.map((items) => (
+            <div
+              key={items.id}
+              className="
+            w-full sm:w-[48%] lg:w-[30%]
+            rounded-2xl p-3 overflow-hidden shadow
+            transform transition-transform duration-300
+            hover:-translate-y-2
+          "
+            >
+              <div className="flex flex-col gap-2">
+                <h1 className="font-bold">{items.id}</h1>
+                <h1>{items.name}</h1>
+
+                {/* Responsive video */}
+                <div className="w-full aspect-video">
                   <iframe
                     src={items.videolink}
-                    width="300"
-                    height="200"
+                    className="w-full h-full rounded-xl"
                     allowFullScreen
-                    autoPlay= 'false'
                   />
-                  <h1>{items.description}</h1>
                 </div>
-              </div>
-            ))
-          }
 
+                <h1>{items.description}</h1>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
+
   )
 }
 // 1st Create contextName -> createContext()
